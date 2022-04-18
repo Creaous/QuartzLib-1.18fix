132ec288911fb81284a12c9a15cb08a3b79454fe
Mitchell <68230846+Creaous@users.noreply.github.com>
2022-04-18 15:41:01 +0930

    Fix for 1.18.2

--- src/main/java/fr/zcraft/quartzlib/components/nbt/NBT.java
+++ src/main/java/fr/zcraft/quartzlib/components/nbt/NBT.java
@@ -301,14 +301,19 @@ public abstract class NBT {
         try {
             Object tagCompound;
             try {
-                //1.18
-                tagCompound = Reflection.call(mcItemStack.getClass(), mcItemStack, "t");
-            } catch (Exception e) {
-                //1.17
+                // 1.18.2
+                tagCompound = Reflection.call(mcItemStack.getClass(), mcItemStack, "u");
+            } catch (Exception e1) {
                 try {
-                    tagCompound = Reflection.call(mcItemStack.getClass(), mcItemStack, "getTag");
-                } catch (Exception e2) {
-                    tagCompound = Reflection.call(mcItemStack.getClass(), mcItemStack, "a");
+                    // 1.18
+                    tagCompound = Reflection.call(mcItemStack.getClass(), mcItemStack, "t");
+                } catch (Exception e) {
+                    // 1.17
+                    try {
+                        tagCompound = Reflection.call(mcItemStack.getClass(), mcItemStack, "getTag");
+                    } catch (Exception e2) {
+                        tagCompound = Reflection.call(mcItemStack.getClass(), mcItemStack, "a");
+                    }
                 }
             }
 
